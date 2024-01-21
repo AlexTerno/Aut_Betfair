6ff63f8476c4b7248d6f7a701b277cea4547e8d3
alexandruternovan <ternovan.alex@gmail.com>
2024-01-21 13:42:31 +0200

    2nd Homework

---
+++ src/main/java/Homework/Library.java
@@ -0,0 +1,44 @@
+package Homework;
+
+class Book {
+    private String name;
+    private int year;
+    private Author author;
+    private double price;
+
+    public Book(String name, int year, Author author, double price) {
+        this.name = name;
+        this.year = year;
+        this.author = author;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
+public class Library {
+    public static void main(String[] args) {
+        // Create an author
+        Author author = new Author("Alex Ternovan", "alex.ternovan@fanduel.com");
+
+        // Create a book
+        Book book = new Book("The Nothingness", 2024, author, 99.99);
+
+        // Print book details
+        System.out.println("Book " + book.getName() + " (" + book.getPrice() + " RON), by " +
+                book.getAuthor().getName() + ", published in " + book.getYear());
+    }
+}
